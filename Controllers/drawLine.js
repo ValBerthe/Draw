2de21cda028e6f2d1e055e1eb8377b44053ecd2b
Adrian Amigues <adrian.amigues@gmail.com>
2016-04-20 17:45:10 +0200

    First drawing on canvas with EaslJS

--- Controllers/drawLine.js
+++ Controllers/drawLine.js
@@ -40,3 +40,17 @@ var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext("2d");
 
 window.addEventListener("mousedown", predraw);			// Quand on clique, la fonction de predraw s'active
+
+
+// Adrian
+
+function init() {
+	var stage = new createjs.Stage("canvas");
+	var circle = new createjs.Shape();
+	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
+	circle.x = 100;
+	circle.y = 100;
+	stage.addChild(circle);
+
+	stage.update();
+}
